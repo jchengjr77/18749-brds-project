c3aae31b57500b1e2f89f923d24da8ecfee299e9
Jonathan Cheng <jonathanchengjr77@gmail.com>
2022-10-30 15:05:23 -0400

    passive replica code drafted

--- server/main.go
+++ server/main.go
@@ -14,7 +14,13 @@ import (
 const (
 	SERVER_TYPE = "tcp"
 	PORT = "8080"
+	SVR_PORT = "8082"
 )
+
+type Pair struct {
+	First int
+	Second int
+}
 
 func printMsg(clientID int, serverID int, msg string, msgType string) {
 	var action string
@@ -59,6 +65,34 @@ func handleClient(conn net.Conn, clientID int, serverID int, stateChan chan int)
 	}
 }
 
+func handlePrimary(conn net.Conn, cpChan chan Pair) {
+	fmt.Println("New Primary Replica Detected.")
+	for {
+		buf := make([]byte, 1024)
+		mlen, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading: ", err.Error())
+			return
+		}	
+		msg := string(buf[:mlen])
+		fmt.Println("[%s] received checkpoint %s", time.Now().Format(time.RFC850), msg)
+
+		// checkpoint format <checkpoint num>,<state>
+		parts := strings.Split(msg, ",")
+		cpNum, err := strconv.Atoi(parts[0])
+		if err != nil {
+			fmt.Println("Error converting using Atoi: ", err.Error())
+			continue
+		}
+		cpState, err := strconv.Atoi(parts[1])
+		if err != nil {
+			fmt.Println("Error converting using Atoi: ", err.Error())
+			continue
+		}
+		cpChan <- Pair{cpNum, cpState}
+	}
+}
+
 func listenerChannelWrapper(listener net.Listener, newClientChan chan net.Conn) {
 	for {
 		conn, err := listener.Accept()
@@ -68,6 +102,18 @@ func listenerChannelWrapper(listener net.Listener, newClientChan chan net.Conn) 
 			return
 		}
 		newClientChan <- conn
+	}
+}
+
+func primaryReplicaListenerWrapper(listener net.Listener, newPrimaryChan chan net.Conn) {
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			// handle connection error
+			fmt.Println("Error accepting: ", err.Error())
+			return
+		}
+		newPrimaryChan <- conn
 	}
 }
 
@@ -114,7 +160,7 @@ func setState(my_state *int, val int) {
 func main() {
 	args := os.Args[1:]
 	if len(args) < 1 {
-		fmt.Printf("Usage: go run server/main.go <server id>")
+		fmt.Printf("Usage: go run server/main.go <server id> <checkpoint freq> <primary?> <backup replica hosts>")
 		return
 	}
 	serverId, err := strconv.Atoi(args[0])
@@ -122,8 +168,28 @@ func main() {
 		fmt.Println("Error parsing args: ", err.Error())
 		return
 	}
+	// checkpointFreq, err := strconv.Atoi(args[1])
+	// if err != nil {
+	// 	fmt.Println("Error parsing args: ", err.Error())
+	// 	return
+	// }
+	// isPrimary := (len(args) >= 3)
+	// backupHostnames := args[3:]
+
+	// TODO: only send checkpoints messages to other replicas if primary
+	/***
+		- uncomment the codelines above. (I needed to silence the "declared but not used" warning)
+		- if server is primary replica, use checkpointFreq to send checkpoint messages.
+		- the primary replica will have to dial() into the two backup replicas. See the client codebase for an example of this. (I defined the backup replica ports to be 8082)
+		- iterate through the backupHostnames and dial into each one at port 8082
+		- you can check arg[3], or isPrimary, to see if this server is the primary replica
+		- checkpoint_freq should be in seconds
+		- every time a new checkpoint message is sent, increment checkpoint_count
+		- checkpoint messages are of the format "<checkpointCount>,<state>"
+	***/
 
 	my_state := 0
+	my_checkpoint_count := 1
 	fmt.Printf("---------- Server %d started ----------\n", serverId)
 
 	// client IDs, monotonically increasing
@@ -138,8 +204,20 @@ func main() {
 	// make channel for new incrementing state
 	stateChan := make(chan int)
 
-	// start the server
+	// make channels for new primary replica messages
+	newPrimaryChan := make(chan net.Conn)
+	checkpointChan := make(chan Pair) // (checkpoint num, state)
+
+	// start the server to receive clients
 	listener, err := net.Listen(SERVER_TYPE, ":"+PORT)
+	if err != nil {
+		// handle server initialization error
+		fmt.Println("Error initializing: ", err.Error())
+		return
+	}
+
+	// listen for new primary replica if needed
+	primaryReplicaListener, err := net.Listen(SERVER_TYPE, ":"+SVR_PORT)
 	if err != nil {
 		// handle server initialization error
 		fmt.Println("Error initializing: ", err.Error())
@@ -153,14 +231,10 @@ func main() {
 	}
 	go listenLFD(lfdConn)
 	go listenerChannelWrapper(listener, newClientChan)
+	go primaryReplicaListenerWrapper(primaryReplicaListener, newPrimaryChan)
 
 	defer listener.Close()
-
-	/**
-	1. spawn a listener thread
-	2. create a channel for new connections [between main thread and listener thread]
-	3. new select/case block in the main thread waits for new connections OR client death notifications
-	**/
+	defer primaryReplicaListener.Close()
 
 	for {
 		select {
@@ -170,6 +244,14 @@ func main() {
 			go handleClient(conn, clientID, serverId, stateChan)
 			clients[clientID] = conn
 			clientID++
-		}
+		case pair := <-checkpointChan:
+			cpNum := pair.First
+			cpState := pair.Second
+			fmt.Printf("[%s] received checkpoint %d -> %d, state %d -> %d \n", time.Now().Format(time.RFC850), my_checkpoint_count, cpNum, my_state, cpState)	
+			my_checkpoint_count = cpNum
+			my_state = cpState
+		case conn := <-newPrimaryChan:
+			go handlePrimary(conn, checkpointChan)
+		} 
 	}
 }
